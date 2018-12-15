0af2bddc16c4d956c36e55e35345b0947c6dea58
Denny Zhang <denny.zhang001@gmail.com>
2018-12-15 10:58:51 -0800

    update doc

--- code/update_git_code.sh
+++ code/update_git_code.sh
@@ -36,7 +36,7 @@ function update_bitbucket(){
 
 function update_github(){
     local code_dir=${1?}
-    repo_list="devops_public code-with-docker chatops_slack cleanup_old_files dennytest detect_suspicious_process developer-free-saas devops_docker_image devops_jenkins directory-cli-tool droplet-neighbor-check elasticsearch-cli-tool images jenkins_image monitor-docker-slack monitoring nmap-scan-docker node_status_in_json python-hosts-tool python-selenium remote-commands-servers shadowsocks-vpn-docker sonarqube-by-docker terraform_jenkins_digitalocean today-learning my-slides challenges-aws-ecs"
+    repo_list="devops_public code-with-docker chatops_slack cleanup_old_files dennytest detect_suspicious_process developer-free-saas devops_docker_image devops_jenkins directory-cli-tool droplet-neighbor-check elasticsearch-cli-tool images jenkins_image monitor-docker-slack monitoring nmap-scan-docker node_status_in_json python-hosts-tool python-selenium remote-commands-servers shadowsocks-vpn-docker sonarqube-by-docker terraform_jenkins_digitalocean today-learning my-slides challenges-aws-ecs kubernetes-scripts"
     [ -d "$code_dir/github" ] || mkdir -p "$code_dir/github"
     cd "$code_dir/github"
     for repo in ${repo_list[*]}; do
